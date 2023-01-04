1bb4a8ec7f05921b17ddbca6fe1cb2a443868773
ApregiPrataYuda <apregipratayda@gmail.com>
2023-01-04 18:18:49 +0700

    add

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -16,9 +16,66 @@ class Auth extends CI_Controller {
 
     public function Process()
     {
-       $username = $this->input->post();
-       $password = $this->input->post();
+        //ambil semua inputan dari user 
+       $username = $this->input->post('username');
+       $password = $this->input->post('password');
 
+       //cek username dari db models apakah ada / tidak
+       $checkusrname = $this->LoginModels->checkusername($username);
+     
+       //jika username ada
+       if ($checkusrname->num_rows() > 0) {
        
+         //cek username dan pass dari db models apakah ada / tidak
+       $checkusernamedanpass = $this->LoginModels->checkusernamedanpass($username,  $password);
+
+       //jika username dan pw ada 
+         if ($checkusernamedanpass->num_rows() > 0) {
+          
+          // ambil semua data username dan pass yang ada 
+            $getallfield = $checkusernamedanpass->row_array();
+
+            //ambil field user_status
+            //jika user statusnya aktif maka boleh masuk
+              if ($getallfield['user_status'] == 1) {
+                
+                //buat session untuk mencegah masuk lewat url
+                $this->session->set_userdata('locked', TRUE);
+
+                //deklarasi
+                $this->session->set_userdata('user', $username);
+
+                //deklarasi id
+                $id = $getallfield['user_id'];
+
+                //jika
+                if ($getallfield['user_akses'] == 1) {
+                   $name =  $getallfield['name'];
+                   $usernames =  $getallfield['username'];
+                   $this->session->set_userdata('access', 'Admin');
+                   $this->session->set_userdata('id', $id);
+                   $this->session->set_userdata('nama', $name);
+                   $this->session->set_userdata('username', $usernames);
+                    redirect('Dashboard');
+                }
+                
+                else {
+                    echo'tidak memiliki hak akses ke page ini';
+                }
+
+
+
+                //jika user_status 0 maka 
+              }else {
+                echo'Akun belum aktif';
+              }
+         //jika pass salah/tidak ada
+         }else{
+            echo'password salah';
+         }
+         //jika username tidak ada
+       }else {
+        echo'username tidak ada';
+       }
     }
 }
--- application/models/LoginModels.php
+++ application/models/LoginModels.php
@@ -3,8 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class LoginModels extends CI_Model {
 
-	public function index()
+	public function checkusername($username)
 	{
-		$this->load->view('welcome_message');
+		$query =  $this->db->query("SELECT * FROM users_login WHERE username = '$username' LIMIT 1");
+        return $query;
+	}
+
+
+    public function checkusernamedanpass($username,$password)
+	{
+		$query =  $this->db->query("SELECT * FROM users_login WHERE username = '$username' AND password = sha1('$password') LIMIT 1");
+        return $query;
 	}
 }
